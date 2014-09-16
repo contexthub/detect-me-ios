7582c9981f0561bd6bb13714fbc65ff5e488fd51
Jeff Kibuule <jeff.kibuule@outlook.com>
2014-09-16 11:51:40 -0500

    Update for iOS 8

--- DetectMe/DMAppDelegate.m
+++ DetectMe/DMAppDelegate.m
@@ -14,15 +14,17 @@
 
 @implementation DMAppDelegate
             
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
-    // Register with ContextHub
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 #ifdef DEBUG
-    // This tells ContextHub that you are running a debug build.
+    // The debug flag is automatically set by the compiler, indicating which push gateway server your device will use
+    // Xcode deployed builds use the sandbox/development server
+    // TestFlight/App Store builds use the production server
+    // ContextHub records which environment a device is using so push works properly
+    // This must be called BEFORE [ContextHub registerWithAppId:]
     [[ContextHub sharedInstance] setDebug:TRUE];
 #endif
     
-    //Register the app id of the application you created on https://app.contexthub.com
+    // Register the app id of the application you created on https://app.contexthub.com
     [ContextHub registerWithAppId:@"YOUR-BEACON-APP-ID-HERE"];
     
     //Set the app delegate as the Datasource and Delegate of the Sensor Pipeline so that we can tap into the events.
@@ -30,7 +32,7 @@
     [[CCHSensorPipeline sharedInstance] setDataSource:self];
     
     //This tells ContextHub about the tags you will use to identify the Beacons that you want to automatically monitor.
-    if (![[CCHSensorPipeline sharedInstance] addSubscriptionForTags:@[DMBeaconTag]]) {
+    if (![[CCHSensorPipeline sharedInstance] addElementsWithTags:@[DMBeaconTag]]) {
         NSLog(@"DM: Failed to add subscription to \"%@\" tag", DMBeaconTag);
     } else {
         NSLog(@"DM: Subscribed to tag \"%@\" tag", DMBeaconTag);
--- release_notes.md
+++ release_notes.md
@@ -1,10 +1,16 @@
 DetectMe (Beacons) Release Notes
+---
 
-## 1.1.2 (Build 228) - August 17, 2014
+## v1.1.3 (Build 241) - September 16, 2014
+- Update to ContextHub v1.3.1 framework
+- Update compatibility for iOS 8 
+- Add NSLocationAlwaysUsageDescription key and description to Info.plist
+
+## v1.1.2 (Build 228) - August 17, 2014
 - Update to ContextHub v1.2.0 framework
 - Add Table of Contents to README
 
-## 1.1.1 (Build 227) - August 10, 2014
+## v1.1.1 (Build 227) - August 10, 2014
 - Fix bug where app would crash when updating and deleting beacons
 - Fix bug where proximity state was not changing when "near" or "far"
 - Added better event handling for differences between "beacon_in"/"beacon_out" and "beacon_changed" events
@@ -13,9 +19,9 @@ DetectMe (Beacons) Release Notes
 - Made footer text in About view readable
 - Updated README
 
-## 1.1.0 (Build 181) - August 8, 2014
+## v1.1.0 (Build 181) - August 8, 2014
 - Rewrite sample app to more directly show how to use the SDK (previous version still available in separate branch)
 - Updated README
 
-## 1.0.0 (Build 161) - August 4, 2014
+## v1.0.0 (Build 161) - August 4, 2014
 - Initial release
